d24570740b9b4cff6ad56e1a85bc870f17a82a67
穆秋实 <gintamatama0912@gmail.com>
2017-01-15 16:42:43 +0800

    first commit

---
+++ GinPrivateSpecLibraryOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GinPrivateSpecLibraryOne
+//
+//  Created by 穆秋实 on 17/1/15.
+//  Copyright © 2017年 穆秋实. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
